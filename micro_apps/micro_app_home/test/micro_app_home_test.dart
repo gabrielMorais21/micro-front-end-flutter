0c481db4c1c3dfc838d2fe5b5428ddcafc57ec69
Gabriel Morais Dias <gabriel.Dias@terceirizadoraizen.com>
2024-02-05 14:13:42 -0300

    teste

--- micro_apps/micro_app_home/test/micro_app_home_test.dart
+++ micro_apps/micro_app_home/test/micro_app_home_test.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("teste1", () {
+  test("teste1 home", () {
     var a = 2 + 2;
     expect(a, 5);
+  });
+
+    test("teste2 home", () {
+    var a = 2 + 2;
+    expect(a, 4);
   });
 }
